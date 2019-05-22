a955909c761a93fb2de61c6bb333650dde059843
Marc Pomar <marc@faable.com>
2019-05-22 21:15:14 +0200

    done

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,41 @@
-const debug = require('debug');
+const debug = require("debug");
 
-let log:any;
+let log: any;
+global.log = undefined;
 
-export const setLog = (prefix:string):any => {
-    log = debug(prefix);
-}
+export const setLog = (prefix: string): any => {
+  global.log = debug(prefix);
+};
 
-export const getLog = (extendLevel:string):any => {
-    if(!log){
-        log = debug(`app:${extendLevel}`);
-    }
-    return log.extend(extendLevel);
-}
+export const getLog = (extendLevel: string): any => {
+  if (!log) {
+    setLog(`app:${extendLevel}`);
+  }
+  return log.extend(extendLevel);
+};
 
 enum LogLevel {
-    DEBUG = 'DEBUG',
-    WARN = 'WARN',
-    ERROR = 'ERROR',
-    LOG = 'LOG'
+  DEBUG = "DEBUG",
+  WARN = "WARN",
+  ERROR = "ERROR",
+  LOG = "LOG"
 }
 
-class LogLevelDecoder {
-    static DEBUG = {label:'debug', color:'blue'};
-    static WARN = {label:'warn', color:'yellow'};
-    static ERROR = {label:'error', color:'red'};
-    static LOG = {label:'log', color:'green'};
+class LogLevelDecoder {
+  static DEBUG = { label: "debug", color: "blue" };
+  static WARN = { label: "warn", color: "yellow" };
+  static ERROR = { label: "error", color: "red" };
+  static LOG = { label: "log", color: "green" };
 }
 
-const getLoggerWithName = (level:LogLevel, logname:string) => (m:string) => getLog(`${LogLevelDecoder[level].label}:${logname}`)(m);
+const getLoggerWithName = (level: LogLevel, logname: string) => (m: string) =>
+  getLog(`${LogLevelDecoder[level].label}:${logname}`)(m);
 
-export const setupLog = (logName:string) => {
-    return {
-        debug: getLoggerWithName(LogLevel.DEBUG, logName),
-        log: getLoggerWithName(LogLevel.LOG, logName),
-        warn: getLoggerWithName(LogLevel.WARN, logName),
-        err: getLoggerWithName(LogLevel.ERROR, logName)
-    }
-}
+export const setupLog = (logName: string) => {
+  return {
+    debug: getLoggerWithName(LogLevel.DEBUG, logName),
+    log: getLoggerWithName(LogLevel.LOG, logName),
+    warn: getLoggerWithName(LogLevel.WARN, logName),
+    err: getLoggerWithName(LogLevel.ERROR, logName)
+  };
+};
